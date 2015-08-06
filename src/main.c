d638f1f762eab7d475893c4506feba2bee93713f
Dudi93 <kamilek93@gmail.com>
2015-08-06 14:32:04 +0200

    working ping pong

--- src/main.c
+++ src/main.c
@@ -12,19 +12,24 @@ int test = 0;
 
 int main(int argc, char* argv[])
 {
+	if (argc != 2)
+	{
+		printf("Wrong parameters\n");
+		return 1;
+	}
 	pthread_t thread1, thread2;
 	thread1 = thread2 = 0;
 	pthread_mutex_init(&MTX, 0);
 	pthread_cond_init(&COND, 0);
 	int* number_of_iterations = malloc(sizeof(int));
-	*number_of_iterations = atoi(argv[1]) / 2;
-	
+	*number_of_iterations = atoi(argv[1]);
 	pthread_create(&thread1, NULL, write_ping, (void*) number_of_iterations);
-	pthread_create(&thread2, NULL, write_ping, (void*) number_of_iterations);
+	pthread_create(&thread2, NULL, write_pong, (void*) number_of_iterations);
 
 	pthread_join(thread1, 0);
 	pthread_join(thread2, 0);
 
+	free(number_of_iterations);
 	return 0;
 }
 
@@ -35,13 +40,12 @@ void* write_ping(void* iterations)
 	for (i = 0; i < *number; ++i)
 	{
 		ping();
-		pong();
 	}	
 
 	return NULL;
 }
 
-/*void* write_pong(void* iterations)
+void* write_pong(void* iterations)
 {
 	int* number = (void*) iterations;
 	int i;
@@ -51,36 +55,27 @@ void* write_ping(void* iterations)
 	}	
 
 	return NULL;
-}*/
+}
 void ping(void)
 {
 	pthread_mutex_lock(&MTX);
 	//printf("%s\n", "PING");
-	if(test == 0)
-	{
-		printf("%s\n", "PING");
-		test = 1;
-		pthread_cond_signal(&COND);
-	}
-	else
+	while (test != 0)
 		pthread_cond_wait(&COND, &MTX);
+	printf("%s\n", "PING");
+	test = 1;
+	pthread_cond_signal(&COND);
 	pthread_mutex_unlock(&MTX);
-
-
 }
 
 void pong(void)
 {
 	pthread_mutex_lock(&MTX);
 	//printf("%s\n", "PONG");
-	if (test == 1)
-	{
-		printf("%s\n", "PONG");
-		test = 0;
-		pthread_cond_signal(&COND);
-	}
-	else
+	while (test != 1)
 		pthread_cond_wait(&COND, &MTX);
+	printf("%s\n", "PONG");
+	test = 0;
+	pthread_cond_signal(&COND);
 	pthread_mutex_unlock(&MTX);
-
 }
